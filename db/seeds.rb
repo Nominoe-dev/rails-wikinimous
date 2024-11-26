df2ed429b95fb6b9bd0c539a370fdfec1c21ea9c
Nominoe Girard <nominoe.girard@gmail.com>
2024-11-26 16:20:33 -0300

    wikinimous done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+# Clear existing data
+Article.destroy_all
+
+# Generate 10 fake articles
+10.times do
+  Article.create(
+    title: Faker::Book.title,    # Generate a fake title
+    content: Faker::Lorem.paragraph(sentence_count: 5) # Generate fake content
+  )
+end
+
+puts "10 fake articles created!"
